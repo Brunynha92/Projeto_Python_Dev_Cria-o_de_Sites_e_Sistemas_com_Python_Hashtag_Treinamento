b5438c131e21aaf85129204d997865fe70d1b93c
Bruna Lima <63168096+Brunynha92@users.noreply.github.com>
2025-09-21 16:52:36 -0300

    Update gabarito.py

--- Python_Dev/gabarito.py
+++ Python_Dev/gabarito.py
@@ -10,7 +10,7 @@
 import streamlit as st
 from openai import OpenAI
 
-modelo = OpenAI(api_key="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")
+modelo = OpenAI(api_key="insira sua chave")
 
 st.write("### ChatBot com IA") # markdown
 
@@ -48,5 +48,6 @@ if mensagem_usuario:
     st.chat_message("assistant").write(resposta_ia)
     mensagem_ia = {"role": "assistant", "content": resposta_ia}
     st.session_state["lista_mensagens"].append(mensagem_ia)
+
 
 
